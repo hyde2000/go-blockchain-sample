8ddd8380581a994e567dddb5e6c034d0136d1132
hyde2000 <hidenorishio@icloud.com>
2022-01-04 12:18:46 +0900

    define wallet model

--- main.go
+++ main.go
@@ -1,17 +1,12 @@
 package main
 
-import "go-blockchain-sample/models"
+import (
+	"fmt"
+	"go-blockchain-sample/models"
+)
 
 func main() {
-	myAddress := "my_blockchain_address"
-	blockchain := models.NewBlockchain(myAddress)
-
-	blockchain.AddTransaction("A", "B", 1.0)
-	blockchain.Mining()
-	blockchain.Print()
-
-	blockchain.AddTransaction("C", "D", 2.0)
-	blockchain.AddTransaction("X", "Y", 3.0)
-	blockchain.Mining()
-	blockchain.Print()
+	w := models.NewWallet()
+	fmt.Println(w.PrivateKeyStr())
+	fmt.Println(w.PublicKeyStr())
 }
---
+++ models/wallet.go
@@ -0,0 +1,38 @@
+package models
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"fmt"
+)
+
+type Wallet struct {
+	privateKey *ecdsa.PrivateKey
+	publicKey  *ecdsa.PublicKey
+}
+
+func NewWallet() *Wallet {
+	privateKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+
+	return &Wallet{
+		privateKey: privateKey,
+		publicKey:  &privateKey.PublicKey,
+	}
+}
+
+func (w *Wallet) PrivateKey() *ecdsa.PrivateKey {
+	return w.privateKey
+}
+
+func (w *Wallet) PrivateKeyStr() string {
+	return fmt.Sprintf("%x", w.privateKey.D.Bytes())
+}
+
+func (w *Wallet) PublicKey() *ecdsa.PublicKey {
+	return w.publicKey
+}
+
+func (w *Wallet) PublicKeyStr() string {
+	return fmt.Sprintf("%x%x", w.publicKey.X.Bytes(), w.publicKey.Y.Bytes())
+}
