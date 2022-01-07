1436569406e1647f7d0946a7655d4f2d4f9f6b70
hyde2000 <hidenorishio@icloud.com>
2022-01-07 11:15:32 +0900

    decode ecdsa struct data

--- utils/ecdsa.go
+++ utils/ecdsa.go
@@ -1,7 +1,11 @@
 package utils
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"encoding/hex"
 	"fmt"
+	"log"
 	"math/big"
 )
 
@@ -11,5 +15,42 @@ type Signature struct {
 }
 
 func (s *Signature) String() string {
-	return fmt.Sprintf("%x%x", s.R, s.S)
+	return fmt.Sprintf("%064x%064x", s.R, s.S)
+}
+
+func String2BigIntTuple(s string) (big.Int, big.Int) {
+	var bix, biy big.Int
+	bx, err := hex.DecodeString(s[:64])
+	if err != nil {
+		log.Println("ERROR: decode failed", err)
+	}
+	by, err := hex.DecodeString(s[64:])
+	if err != nil {
+		log.Println("ERROR: decode failed", err)
+	}
+	_ = bix.SetBytes(bx)
+	_ = biy.SetBytes(by)
+
+	return bix, biy
+}
+
+func PublicKeyFromString(s string) *ecdsa.PublicKey {
+	x, y := String2BigIntTuple(s)
+
+	return &ecdsa.PublicKey{Curve: elliptic.P256(), X: &x, Y: &y}
+}
+
+func PrivateKeyFromString(s string, publicKey *ecdsa.PublicKey) *ecdsa.PrivateKey {
+	var bi big.Int
+
+	b, _ := hex.DecodeString(s[:])
+	_ = bi.SetBytes(b)
+
+	return &ecdsa.PrivateKey{PublicKey: *publicKey, D: &bi}
+}
+
+func SignatureFromString(str string) *Signature {
+	r, s := String2BigIntTuple(str)
+
+	return &Signature{R: &r, S: &s}
 }
