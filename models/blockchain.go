e32473ca95e83d08c801f662d83217b178afe7bd
hyde2000 <hidenorishio@icloud.com>
2022-01-12 15:48:08 +0900

    create consensus to resolve conflicts

--- models/blockchain.go
+++ models/blockchain.go
@@ -45,6 +45,10 @@ func NewBlockchain(blockchainAddress string, port uint16) *Blockchain {
 	return bc
 }
 
+func (bc *Blockchain) Chain() []*Block {
+	return bc.chain
+}
+
 func (bc *Blockchain) SetNeighbors() {
 	bc.neighbors = utils.FindNeighbors("127.0.0.1", bc.port, NeighborIpRangeStart, NeighborIpRangeEnd, BlockchainPortRangeStart, BlockchainPortRangeEnd)
 	log.Printf("%v", bc.neighbors)
@@ -200,6 +204,36 @@ func (bc *Blockchain) ValidChain(chain []*Block) bool {
 		currentIndex += 1
 	}
 	return true
+}
+
+func (bc *Blockchain) ResolveConflicts() bool {
+	var longestChain []*Block = nil
+	maxLength := len(bc.chain)
+
+	for _, n := range bc.neighbors {
+		endpoint := fmt.Sprintf("http://%s/chain", n)
+		resp, _ := http.Get(endpoint)
+		if resp.StatusCode == 200 {
+			var bcResp Blockchain
+			decoder := json.NewDecoder(resp.Body)
+			_ = decoder.Decode(&bcResp)
+
+			chain := bcResp.Chain()
+			if len(chain) > maxLength && bc.ValidChain(chain) {
+				maxLength = len(chain)
+				longestChain = chain
+			}
+		}
+	}
+
+	if longestChain != nil {
+		bc.chain = longestChain
+		log.Println("Resolve conflicts")
+		return true
+	}
+
+	log.Println("Resolve conflicts not replaced")
+	return false
 }
 
 func (bc *Blockchain) ProofOfWork() int {
